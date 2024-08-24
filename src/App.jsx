2a99305d7431ecbff39d901c9797cdec1d66ec5d
CC9754-Anas <anasseraj01@gmail.com>
2024-08-24 09:03:00 +0530

    dynamically rendering the list items.

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,16 @@ function App() {
   }
   // console.log(selectedTopic);
   let tabContent = <p>Please select a Topic!</p>;
-  if (selectedTopic){
-    tabContent = <div id="tab-content">
-    <h3>{EXAMPLES[selectedTopic].title}</h3>
-    <p>{EXAMPLES[selectedTopic].description}</p>
-    <pre>
-      <code>{EXAMPLES[selectedTopic].code}</code>
-    </pre>
-  </div>
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
   // appliction 'return' starts from here:
   return (
@@ -29,7 +31,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map(e => (<CoreConcept {...e}/>))}
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
             {/* or we can use 'CoreConcept' component for each element in 'CORE_CONCEPTS' */}
             {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
@@ -44,16 +48,28 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect("components")}>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
               {EXAMPLES.components.title}
             </TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect("jsx")}>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
               {EXAMPLES.jsx.title}
             </TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect("props")}>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
               {EXAMPLES.props.title}
             </TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect("state")}>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
               {EXAMPLES.state.title}
             </TabButton>
           </menu>
@@ -69,13 +85,15 @@ function App() {
           ) : null} */}
           {/* or we can do this: */}
           {!selectedTopic && <p>Please select a Topic!</p>}
-          {selectedTopic && <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>}
+          {selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
           {/* or we can also just use a variable such as: */}
           {/* {tabContent} */}
         </section>
